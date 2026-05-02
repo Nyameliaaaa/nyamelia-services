a1f6f2e45d80b20d7dfe0f3909b3f4aacfb485b1
Amelia Rose <nyameliaaaa@proton.me>
2026-05-02 10:31:52 +0300

    btoa moment

--- src/lastFM.ts
+++ src/lastFM.ts
@@ -14,6 +14,16 @@ export const cachedFetch = async (kv: KVNamespace, key: string, url: string, ttl
     return data;
 };
 
+const cacheKey = async (prefix: string, ...parts: string[]) => {
+    const text = parts.join(':');
+    const hash = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(text));
+    const hex = Array.from(new Uint8Array(hash))
+        .map(b => b.toString(16).padStart(2, '0'))
+        .join('');
+
+    return `${prefix}:${hex}`;
+};
+
 export const getTrackImage = async (
     artist: string,
     track: string,
@@ -26,7 +36,7 @@ export const getTrackImage = async (
 
     const mbData = await cachedFetch(
         c.env.lastfm_cache,
-        btoa(`${artist}/${track}`),
+        await cacheKey('mb:track', artist, track),
         `https://musicbrainz.org/ws/2/release/?query=artist:${encodeURIComponent(artist)}+release:${encodeURIComponent(track)}&fmt=json&limit=1`,
         604800
     );
@@ -51,7 +61,7 @@ export const getAlbumImage = async (
 
     const mbData = await cachedFetch(
         c.env.lastfm_cache,
-        btoa(`${artist}/${album}`),
+        await cacheKey('mb:track', artist, album),
         `https://musicbrainz.org/ws/2/release/?query=artist:${encodeURIComponent(artist)}+release:${encodeURIComponent(album)}&fmt=json&limit=1`,
         604800
     );
